6413f5cee46b06e9e6f2bd8a621dfebff64c27fe
Grady Link <github@grady.link>
2025-06-08 13:28:34 -0700

    very very basic transpiling

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go.grady.link/LIT
 
 go 1.24.3
+
+require github.com/dave/jennifer v1.7.1 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"github.com/dave/jennifer/jen"
+)
 
 func main() {
 	project, err := Parse("./test-extracted/project.json")
@@ -8,5 +10,42 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", project)
+	f := jen.NewFile("main")
+	for _, target := range project.Targets {
+		if target.IsStage {
+			continue
+		}
+
+		for _, block := range target.Blocks {
+			if block.Opcode != "procedures_definition" {
+				continue
+			}
+			if *target.Blocks[*block.Inputs["custom_block"].BlockID].Mutation.ProcCode == "Main" {
+				var statements []jen.Code
+				next := block.Next
+				for {
+					switch target.Blocks[*next].Opcode {
+					case "looks_say":
+						switch (*target.Blocks[*next].Inputs["MESSAGE"].Value).(type) {
+						case string:
+							statements = append(statements, jen.Qual("fmt", "Println").Call(jen.Lit(*target.Blocks[*next].Inputs["MESSAGE"].Value)))
+						default:
+							panic("Unsupported Input Type")
+						}
+					default:
+						panic("Unsupported Opcode: " + target.Blocks[*next].Opcode)
+					}
+					if target.Blocks[*next].Next == nil {
+						break
+					}
+					next = target.Blocks[*next].Next
+				}
+				f.Func().Id("main").Params().Block(statements...)
+			}
+		}
+	}
+
+	if err = f.Save("dist/main.go"); err != nil {
+		panic(err)
+	}
 }
