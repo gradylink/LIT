5cc28de5ab7d83358a8b908d33e98e9985e14aa6
Grady Link <github@grady.link>
2025-06-22 10:44:12 -0700

    chore: add todos

--- motion.go
+++ motion.go
@@ -6,8 +6,8 @@ func Motion(block Block) []jen.Code {
 	switch block.Opcode {
 	case "motion_movesteps":
 		return []jen.Code{
-			jen.Id("t").Dot("X").Op("+=").Lit(100).Op("*").Qual("math", "Cos").Call(jen.Parens(jen.Lit(90).Op("-").Id("t").Dot("Direction")).Op("*").Lit(180).Op("/").Qual("math", "Pi")),
-			jen.Id("t").Dot("Y").Op("+=").Lit(100).Op("*").Qual("math", "Sin").Call(jen.Parens(jen.Lit(90).Op("-").Id("t").Dot("Direction")).Op("*").Lit(180).Op("/").Qual("math", "Pi")),
+			jen.Id("t").Dot("X").Op("+=").Lit(100 /* TODO: remove hard-coded value */).Op("*").Qual("math", "Cos").Call(jen.Parens(jen.Lit(90).Op("-").Id("t").Dot("Direction")).Op("*").Lit(180).Op("/").Qual("math", "Pi")),
+			jen.Id("t").Dot("Y").Op("+=").Lit(100 /* TODO: remove hard-coded value */).Op("*").Qual("math", "Sin").Call(jen.Parens(jen.Lit(90).Op("-").Id("t").Dot("Direction")).Op("*").Lit(180).Op("/").Qual("math", "Pi")),
 			jen.Return(jen.Lit(true)),
 		}
 	}
