d512d0f62c92e420f921e372843d13ced6056aa9
Grady Link <github@grady.link>
2025-06-26 16:27:31 -0700

    refactor: improve logic of input parser

--- inputs.go
+++ inputs.go
@@ -8,15 +8,20 @@ import (
 
 func ParseInput(i Input, toNumber bool) (jen.Code, error) {
 	if i.Type == 1 {
-		if toNumber {
-			num, err := strconv.ParseFloat((*i.Value).(string), 64)
-			if err != nil {
-				return nil, err
-			}
+		switch (*i.Value).(type) {
+		case float64:
+			return jen.Lit(*i.Value), nil
+		case string:
+			if toNumber {
+				num, err := strconv.ParseFloat((*i.Value).(string), 64)
+				if err != nil {
+					return nil, err
+				}
 
-			return jen.Lit(num), nil
+				return jen.Lit(num), nil
+			}
+			return jen.Lit(*i.Value), nil
 		}
-		return jen.Lit(*i.Value), nil
 	}
 	if i.BroadcastID != nil {
 		return jen.Lit(*i.BroadcastID), nil
